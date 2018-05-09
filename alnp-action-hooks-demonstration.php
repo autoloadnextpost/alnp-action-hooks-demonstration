5bdbf34a059ec49871b0c2d5f5e8e3a00a42bb3f
Sébastien Dumont <mailme@sebastiendumont.com>
2018-05-09 02:33:36 +0200

    Updated hooks and added two new ones.

--- alnp-action-hooks-demonstration.php
+++ alnp-action-hooks-demonstration.php
@@ -2,23 +2,27 @@
 /*
  * Plugin Name: Auto Load Next Post: Action Hooks Demonstration
  * Plugin URI: https://github.com/AutoLoadNextPost/alnp-action-hooks-demonstration
- * Description: Demonstrates how to add content before and after the loop and content of the next loaded post that is called. Requires v1.4.1 of Auto Load Next Post.
- * Version: 1.2.0
+ * Description: Demonstrates how to add content before and after the loop and content of the next loaded post that is called.
  * Author: Auto Load Next Post
  * Author URI: https://autoloadnextpost.com
+ * Version: 1.2.1
  * Developer: Sébastien Dumont
  * Developer URI: https://sebastiendumont.com
- * GitHub Plugin URI: https://github.com/AutoLoadNextPost/alnp-action-hooks-demonstration
- *
  * Text Domain: alnp-action-hooks-demonstration
  * Domain Path: /languages/
  *
- * Requires at least: 4.5
- * Tested up to: 4.9.1
+ * GitHub Plugin URI: https://github.com/AutoLoadNextPost/alnp-action-hooks-demonstration
  *
  * Copyright: © 2018 Sébastien Dumont
+ *
  * License: GNU General Public License v3.0
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package   Auto Load Next Post: Action Hooks Demonstration
+ * @author    Auto Load Next Post
+ * @copyright Copyright © 2018, Auto Load Next Post
+ * @license   GNU General Public License v3.0 http://www.gnu.org/licenses/gpl-3.0.html
+ *
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -37,28 +41,30 @@ function alnp_hook_demo_after_box( $content ) {
 $action_hooks = array(
 	'load_before_loop',
 	'load_before_content',
-	'load_before_content_type_standard',
-	'load_before_content_type_aside',
-	'load_before_content_type_chat',
-	'load_before_content_type_gallery',
-	'load_before_content_type_link',
-	'load_before_content_type_image',
-	'load_before_content_type_quote',
-	'load_before_content_type_status',
-	'load_before_content_type_video',
-	'load_before_content_type_audio',
+	'load_before_content_post_format_standard',
+	'load_before_content_post_format_aside',
+	'load_before_content_post_format_chat',
+	'load_before_content_post_format_gallery',
+	'load_before_content_post_format_link',
+	'load_before_content_post_format_image',
+	'load_before_content_post_format_quote',
+	'load_before_content_post_format_status',
+	'load_before_content_post_format_video',
+	'load_before_content_post_format_audio',
 	'load_after_loop',
 	'load_after_content',
-	'load_after_content_type_standard',
-	'load_after_content_type_aside',
-	'load_after_content_type_chat',
-	'load_after_content_type_gallery',
-	'load_after_content_type_link',
-	'load_after_content_type_image',
-	'load_after_content_type_quote',
-	'load_after_content_type_status',
-	'load_after_content_type_video',
-	'load_after_content_type_audio',
+	'load_after_content_post_format_standard',
+	'load_after_content_post_format_aside',
+	'load_after_content_post_format_chat',
+	'load_after_content_post_format_gallery',
+	'load_after_content_post_format_link',
+	'load_after_content_post_format_image',
+	'load_after_content_post_format_quote',
+	'load_after_content_post_format_status',
+	'load_after_content_post_format_video',
+	'load_after_content_post_format_audio',
+	'load_before_content_post_type_post',
+	'load_after_content_post_type_post',
 	'no_more_posts'
 );
 
@@ -83,53 +89,53 @@ function alnp_hook_demo_load_before_content() {
 }
 
 // Load content before the content for standard posts.
-function alnp_hook_demo_load_before_content_type_standard() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for standard posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_standard</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_standard() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for standard posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_standard</em>' ) );
 }
 
 // Load content before the content for posts without a title.
-function alnp_hook_demo_load_before_content_type_aside() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts without a title!', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_aside</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_aside() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts without a title!', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_aside</em>' ) );
 }
 
 // Load content before the content for chat transcript posts.
-function alnp_hook_demo_load_before_content_type_chat() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for chat transcript posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_chat</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_chat() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for chat transcript posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_chat</em>' ) );
 }
 
 // Load content before the content for posts that contain a gallery.
-function alnp_hook_demo_load_before_content_type_gallery() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain a gallery using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_gallery</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_gallery() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain a gallery using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_gallery</em>' ) );
 }
 
 // Load content before the content for posts that link to another site.
-function alnp_hook_demo_load_before_content_type_link() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that link to another site using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_link</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_link() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that link to another site using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_link</em>' ) );
 }
 
 // Load content before the content for posts that contain an image.
-function alnp_hook_demo_load_before_content_type_image() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain an image using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_image</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_image() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain an image using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_image</em>' ) );
 }
 
 // Load content before the content for posts that contain a quote.
-function alnp_hook_demo_load_before_content_type_quote() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain a quote using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_quote</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_quote() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contain a quote using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_quote</em>' ) );
 }
 
 // Load content before the content for posts that contains a short status update.
-function alnp_hook_demo_load_before_content_type_status() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contains a short status update!', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_status</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_status() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts that contains a short status update!', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_status</em>' ) );
 }
 
 // Load content before the content for posts with a video or a video playlist.
-function alnp_hook_demo_load_before_content_type_video() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts with a video or a video playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_video</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_video() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts with a video or a video playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_video</em>' ) );
 }
 
 // Load content before the content for posts with an audio file or a audio playlist.
-function alnp_hook_demo_load_before_content_type_audio() {
-	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts with an audio file or a audio playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_type_audio</em>' ) );
+function alnp_hook_demo_load_before_content_post_format_audio() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before the content for posts with an audio file or a audio playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_format_audio</em>' ) );
 }
 
 // Load content after the content.
@@ -138,56 +144,66 @@ function alnp_hook_demo_load_after_content() {
 }
 
 // Load content after the content for standard posts.
-function alnp_hook_demo_load_after_content_type_standard() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for standard posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_standard</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_standard() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for standard posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_standard</em>' ) );
 }
 
 // Load content after the content for posts without a title.
-function alnp_hook_demo_load_after_content_type_aside() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts without a title using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_aside</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_aside() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts without a title using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_aside</em>' ) );
 }
 
 // Load content after the content for chat transcript posts.
-function alnp_hook_demo_load_after_content_type_chat() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for chat transcript posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_chat</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_chat() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for chat transcript posts using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_chat</em>' ) );
 }
 
 // Load content after the content for posts that contain a gallery.
-function alnp_hook_demo_load_after_content_type_gallery() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain a gallery using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_gallery</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_gallery() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain a gallery using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_gallery</em>' ) );
 }
 
 // Load content after the content for posts that link to another site.
-function alnp_hook_demo_load_after_content_type_link() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that link to another site using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_link</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_link() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that link to another site using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_link</em>' ) );
 }
 
 // Load content after the content for posts that contain an image.
-function alnp_hook_demo_load_after_content_type_image() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain an image using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_image</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_image() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain an image using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_image</em>' ) );
 }
 
 // Load content after the content for posts that contain a quote.
-function alnp_hook_demo_load_after_content_type_quote() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain a quote using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_quote</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_quote() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contain a quote using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_quote</em>' ) );
 }
 
 // Load content after the content for posts that contains a short status update.
-function alnp_hook_demo_load_after_content_type_status() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contains a short status update using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_status</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_status() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts that contains a short status update using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_status</em>' ) );
 }
 
 // Load content after the content for posts with a video or a video playlist.
-function alnp_hook_demo_load_after_content_type_video() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts with a video or a video playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_video</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_video() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts with a video or a video playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_video</em>' ) );
 }
 
 // Load content after the content for posts with an audio file or a audio playlist.
-function alnp_hook_demo_load_after_content_type_audio() {
-	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts with an audio file or a audio playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_type_audio</em>' ) );
+function alnp_hook_demo_load_after_content_post_format_audio() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after the content for posts with an audio file or a audio playlist using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_format_audio</em>' ) );
 }
 
 // No more content to load.
 function alnp_hook_demo_no_more_posts() {
 	alnp_hook_demo_after_box( sprintf( __( 'This box was added because there are no more posts to load using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_no_more_posts</em>' ) );
+}
+
+// Load content before the content for post type "post".
+function alnp_load_before_content_post_type_post() {
+	alnp_hook_demo_before_box( sprintf( __( 'This box was added before content for post type "post" using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_before_content_post_type_post</em>' ) );
+}
+
+// Load content after the content for post type "post".
+function alnp_load_after_content_post_type_post() {
+	alnp_hook_demo_after_box( sprintf( __( 'This box was added after content for post type "post" using "%s"', 'alnp-action-hooks-demonstration' ), '<em>alnp_load_after_content_post_type_post</em>' ) );
 }
